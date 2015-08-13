b59d17cb029902e39d3d5478cfeabd3f5f44e341
Paul Jolly <paul@myitcv.org.uk>
2015-08-13 14:27:09 +0100

    Use latest best practice for tsc

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,23 @@ var sourcemaps = require("gulp-sourcemaps");
 var ts = require("gulp-typescript");
 var babel = require("gulp-babel");
 var print = require("gulp-print");
+var shell = require("gulp-shell");
 
-var tsProject = ts.createProject("app/tsconfig.json", {typescript: require("typescript")});
+// var babelOptions = {
+// 	modules: "system",
+// 	optional: ["es7.objectRestSpread", "es7.classProperties", "strict"]
+// };
 
-var babelOptions = {
-	modules: "system",
-	optional: ["es7.objectRestSpread", "es7.classProperties", "strict"]
-};
-
-gulp.task("watch", ["ts"], function() {
-	gulp.watch("app/**/*.ts", ["ts"]);
-});
+// gulp.task("watch", ["ts"], function() {
+// 	gulp.watch("app/**/*.ts", ["ts"]);
+// });
 
 gulp.task("default", ["ts"]);
 
-gulp.task('ts', function() {
-	return gulp.src("app/**/*.{ts,tsx}")
-	.pipe(sourcemaps.init())
-	.pipe(print(function(filepath) {
-		return "ts transpiled: " + filepath;
-	}))
-	.pipe(ts(tsProject))
-	.js.pipe(babel(babelOptions))
-	.pipe(sourcemaps.write("."))
-	.pipe(gulp.dest("dist"));
-});
+gulp.task("ts", shell.task([
+	"tsc"
+], {cwd: "app"}));
+
+gulp.task("watch", shell.task([
+	"tsc --watch"
+], {cwd: "app"}));
