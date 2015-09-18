7dde2d20da5c655c6598c9fad1cea0405b6feca5
Paul Jolly <paul@myitcv.org.uk>
2015-09-18 11:13:53 +0100

    Fix travis build

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,6 @@
 var gulp = require('gulp');
-var sourcemaps = require("gulp-sourcemaps");
 var ts = require("gulp-typescript");
-var babel = require("gulp-babel");
-var print = require("gulp-print");
 var shell = require("gulp-shell");
-
-// var babelOptions = {
-// 	modules: "system",
-// 	optional: ["es7.objectRestSpread", "es7.classProperties", "strict"]
-// };
-
-// gulp.task("watch", ["ts"], function() {
-// 	gulp.watch("app/**/*.ts", ["ts"]);
-// });
 
 gulp.task("default", ["ts"]);
 
