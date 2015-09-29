04e57a5d7c546ada57d6a8df220b98835003e655
Paul Jolly <paul@myitcv.org.uk>
2015-09-29 15:15:31 +0100

    Correct ref to immutable type definition

--- app/tsd.d.ts
+++ app/tsd.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="../jspm_packages/npm/immutable@3.7.4/dist/immutable.d.ts" />
+/// <reference path="../jspm_packages/npm/immutable@3.7.5/dist/immutable.d.ts" />
 /// <reference path="../typings/d3/d3.d.ts" />
 /// <reference path="../typings/react/react.d.ts" />
 /// <reference path="../typings/es6-collections/es6-collections.d.ts" />
