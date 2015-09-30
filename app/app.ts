3591e5f33e13d8787fcc415b7c9c7f1c7c3cbafd
Paul Jolly <paul@myitcv.org.uk>
2015-09-30 11:12:54 +0100

    Fix jspm package versions and whitespace fix

--- app/app.ts
+++ app/app.ts
@@ -2,5 +2,5 @@
 
 import * as Immutable from "immutable";
 
-let m: Immutable.Map<string, number> = Immutable.Map({test: 5});
+let m: Immutable.Map<string, number> = Immutable.Map({ test: 5 });
 console.log(m);
