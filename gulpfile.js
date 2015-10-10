ba8303894d1cfc1ed4970b66fa6f70b1c4a1aafa
Paul Jolly <paul@myitcv.org.uk>
2015-10-10 06:06:31 +0100

    Make gulp default watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp');
 var shell = require("gulp-shell");
 
-gulp.task("default", ["ts"]);
-
-gulp.task("ts", shell.task([
-	"tsc"
-], {cwd: "app"}));
+gulp.task("default", ["watch"]);
 
 gulp.task("watch", shell.task([
 	"tsc --watch"
