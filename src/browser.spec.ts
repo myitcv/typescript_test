1b266bf454484150b0c5b425bb5fa5ca84eafdf8
Paul Jolly <paul@myitcv.org.uk>
2015-11-28 14:17:45 +0000

    Upgrade various packages

--- src/browser.spec.ts
+++ src/browser.spec.ts
@@ -1,7 +1,13 @@
 import {Run} from "./browser";
 
 describe("Browser", () => {
+	let container: HTMLElement;
+
+	beforeEach(() => {
+		container = document.createElement("div");
+	});
+
 	it("should work", () => {
-		expect(() => Run()).not.toThrow();
+		expect(() => Run(container)).not.toThrow();
 	});
 });
--- src/browser.ts
+++ src/browser.ts
@@ -1,8 +1,7 @@
-import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {BuildMyComponent} from "./example";
 
-export function Run(): void {
+export function Run(e: Element): void {
 	console.log("Running browser");
-	ReactDOM.render(BuildMyComponent(), document.getElementById("main-content"));
+	ReactDOM.render(BuildMyComponent(), e);
 }
